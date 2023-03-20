55cb3edb1763fc093a4fafbc8bfaecad791e8083
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-03-20 18:06:07 +0900

    jpa
    - 프록시, 값타입

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -20,14 +21,11 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            em.persist(order);
-//            order.addOrderItem(new OrderItem());
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
+            Book book = new Book();
+            book.setName("jpa");
+            book.setAuthor("김영한");
 
-            em.persist(orderItem);
-
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e){
